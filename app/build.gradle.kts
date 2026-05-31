8e5ebfbdfd90aebf36eeb24229f1201729ffcafd
ashlydev99 <ashlydev99@gmail.com>
2026-05-31 05:20:15 -0400

    Last Sync: 2026-05-31 05:20 (Mobile)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
-    // 🔧 Ajustado para JitPack
-    implementation("com.github.mikepenz:MarkdownCompose:0.6.0")
+    // 🔧 Librería correcta de Mike Penz
+    implementation("com.mikepenz:multiplatform-markdown-renderer:0.41.0")
 
     implementation("io.github.ljcamargo:llamacpp-kotlin:0.4.0")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.34.0")
